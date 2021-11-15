112466b4e3d0aaaa29da6f11da909c50e88f464b
darmawan21 <d.darmawan2111@gmail.com>
2021-11-15 13:45:17 +0700

    add edit profile page header

---
+++ lib/pages/edit_profile_page.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:shamo/theme.dart';
+
+class EditProfilePage extends StatelessWidget {
+  const EditProfilePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    PreferredSizeWidget header() {
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        elevation: 0,
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.close,
+          ),
+        ),
+        title: Text(
+          'Edit Profile',
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.check,
+              color: primaryColor,
+            ),
+          ),
+        ],
+      );
+    }
+
+    Widget content() {
+      return Container(
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(
+          horizontal: defaultMargin,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              margin: EdgeInsets.only(
+                top: defaultMargin,
+              ),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/image_profile.png',
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: backgroundColor3,
+      appBar: header(),
+      body: content(),
+    );
+  }
+}
