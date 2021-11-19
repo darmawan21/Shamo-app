0746819d159333b68ab81975f58b1046452d1d40
darmawan21 <d.darmawan2111@gmail.com>
2021-11-19 21:34:29 +0700

    add header and items in checkout page

---
+++ lib/pages/checkout_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:shamo/theme.dart';
+import 'package:shamo/widgets/checkout_card.dart';
+
+class CheckoutPage extends StatelessWidget {
+  const CheckoutPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    PreferredSizeWidget header() {
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        elevation: 0,
+        centerTitle: true,
+        title: Text('Checkout Details'),
+      );
+    }
+
+    Widget content() {
+      return ListView(
+        padding: EdgeInsets.symmetric(
+          horizontal: defaultMargin,
+        ),
+        children: [
+          // NOTE: LIST ITEM
+          Container(
+            margin: EdgeInsets.only(
+              top: defaultMargin,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'List items',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: medium,
+                  ),
+                ),
+                CheckoutCard(),
+                CheckoutCard(),
+              ],
+            ),
+          ),
+        ],
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: backgroundColor3,
+      appBar: header(),
+      body: content(),
+    );
+  }
+}
