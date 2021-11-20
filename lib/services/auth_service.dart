6006433f011cacdf57b56c098e4f271664a8ad1c
darmawan21 <d.darmawan2111@gmail.com>
2021-11-20 13:00:00 +0700

    add auth service

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,41 @@
+// request backend
+import 'dart:convert';
+
+import 'package:shamo/models/user_models.dart';
+import 'package:http/http.dart' as http;
+
+class AuthService {
+  String baseUrl = 'https://shamo-backend.builwithangga.id/api';
+
+  Future<UserModel> register({
+    String? name,
+    String? username,
+    String? email,
+    String? password,
+  }) async {
+    var url = '$baseUrl/register';
+    var header = {'Content-Type': 'application/json'};
+    var body = jsonEncode({
+      'name': name,
+      'username': username,
+      'email': email,
+      'password': password,
+    });
+
+    var response = await http.post(
+      Uri.parse(url),
+      headers: header,
+      body: body,
+    );
+
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body)['data'];
+      UserModel user = UserModel.fromJson(data['user']);
+      user.token = 'Bearer' + data['access_toke'];
+
+      return user;
+    } else {
+      throw Exception('Gagal Register');
+    }
+  }
+}
